d1adc4514b8c11bfb0738dcc40620da21a26a2ba
Kata <35190838+olivabigyo@users.noreply.github.com>
2021-03-23 09:54:08 +0100

    Initialize colors (static)

--- frontend/code.js
+++ frontend/code.js
@@ -4,19 +4,35 @@ const tiles = document.querySelectorAll('.tile');
 let r = 0;
 let c = 0;
 
-// Positioning
+// Static coloring
+// color of the (0,0) tile
+const colorOrigin = [10, 140, 250];
+// rgb color steps
+const colorRowSteps = [20, 10, -30];
+const colorColumnSteps = [15, 25, -20];
+
+// Initialize tiles
 for (const tile of tiles) {
+    // position of the tile
     tile.style.top = `${r * 125}px`;
     tile.style.left = `${c * 100}px`;
     console.log(r, c);
+    // color of the tile
+    const color = [
+        colorOrigin[0] + colorRowSteps[0] * r + colorColumnSteps[0] * c, //red
+        colorOrigin[1] + colorRowSteps[1] * r + colorColumnSteps[1] * c, //green
+        colorOrigin[2] + colorRowSteps[2] * r + colorColumnSteps[2] * c  //blue
+    ];
+    tile.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+
     c++;
     if (c >= 4) {
         r++; c = 0;
     }
 }
 
-// Add Event listeners
-// Tiles are selectable
+// Add Event listeners:
+// Tiles are selectable and change position
 let selected = null;
 
 for (const tile of tiles) {
