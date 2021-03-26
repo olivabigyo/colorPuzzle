728bd911967bbb40b56f7cc6a96c81f3fac68c78
Kata <35190838+olivabigyo@users.noreply.github.com>
2021-03-26 15:22:45 +0100

    Add cors headers

--- backend/server.php
+++ backend/server.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json');
 
 $request = json_decode(file_get_contents('php://input'));
